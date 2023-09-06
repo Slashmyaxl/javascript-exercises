f1c85ab09f401761d15008043367e387f1fe1154
Slashmyaxl <140274090+Slashmyaxl@users.noreply.github.com>
2023-09-06 15:16:03 -0400

    Pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let strung = '';
+    
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 1; i <= num; i++) {
+            strung += `${string}`;
+        }
+    }
+    return strung;
 };
 
 // Do not edit below this line
