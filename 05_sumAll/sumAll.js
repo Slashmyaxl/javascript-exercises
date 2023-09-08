925aa4350f4f009c8279bb157bd78271f110dc98
Slashmyaxl <140274090+Slashmyaxl@users.noreply.github.com>
2023-09-08 13:04:49 -0400

    First commit, pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+
+    if (x < 0 || y < 0 || typeof (x) !== "number" || typeof (y) !== "number") {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+    let values = [];
+    let minValue = Math.min(x, y);
+    let maxValue = Math.max(x, y);
+
+    while (minValue <= maxValue) {
+        values.push(minValue);
+        minValue++;
+    }
+
+    for (let i = 0; i < values.length; i++) {
+        sum += values[i];
+    }
+
+    return sum;
 
 };
 
